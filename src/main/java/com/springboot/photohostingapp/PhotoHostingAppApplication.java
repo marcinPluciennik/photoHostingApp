607dc25a883e450635b7741721700c0afc25e235
marcinPluciennik <marcinpluciennik76@gmail.com>
2021-04-20 11:40:00 +0200

    first commit

---
+++ src/main/java/com/springboot/photohostingapp/PhotoHostingAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.photohostingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoHostingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoHostingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/photohostingapp/PhotoHostingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.photohostingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoHostingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
