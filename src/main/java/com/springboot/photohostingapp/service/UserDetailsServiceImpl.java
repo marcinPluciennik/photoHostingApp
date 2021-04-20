ed7c5228e2de19961b403f5abf01b70307810013
marcinPluciennik <marcinpluciennik76@gmail.com>
2021-04-20 12:19:50 +0200

    Added UserDetailsServiceImpl class

---
+++ src/main/java/com/springboot/photohostingapp/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.springboot.photohostingapp.service;
+
+import com.springboot.photohostingapp.repo.AppUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private AppUserRepo appUserRepo;
+
+    @Autowired
+    public UserDetailsServiceImpl(AppUserRepo appUserRepo) {
+        this.appUserRepo = appUserRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return appUserRepo.findByUsername(s);
+    }
+}
