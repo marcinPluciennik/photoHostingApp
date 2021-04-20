63a11962d1f8ba9705450565ccdf594181c5fae7
marcinPluciennik <marcinpluciennik76@gmail.com>
2021-04-20 12:14:25 +0200

    Added AppUser repository

---
+++ src/main/java/com/springboot/photohostingapp/repo/AppUserRepo.java
@@ -0,0 +1,10 @@
+package com.springboot.photohostingapp.repo;
+
+import com.springboot.photohostingapp.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepo extends JpaRepository<AppUser, Long> {
+    AppUser findByUsername(String username);
+}
